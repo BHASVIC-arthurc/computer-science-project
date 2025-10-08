aa976fccf724b725548e41b4c8364d17e8fb5a5d
BHASVIC-arthurc <arthur.croucher24@students.bhasvic.ac.uk>
2025-10-08 10:01:13 +0100

    fixed minor bugs

--- compsci project/Assets/scripts/player_movement.cs
+++ compsci project/Assets/scripts/player_movement.cs
@@ -5,12 +5,13 @@ public class player_movement : MonoBehaviour
 {
     [SerializeField]  private float acceleration;
     [SerializeField]  private float jumpForce;
-    private float x_speed;
-    private float y_speed;
-    private bool Grounded = true;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private LayerMask groundLayer;
+    private float x_speed;
+    private float y_speed;
+    private bool Grounded = true;
     private bool canDash=true;
     private bool Dashing;
     
@@ -20,6 +21,7 @@ public class player_movement : MonoBehaviour
     }
     void Update()
     {
+        isGrounded();
         if (!Dashing)
         {
             Move();
@@ -36,7 +38,7 @@ public class player_movement : MonoBehaviour
         x_speed = Input.GetAxis("Horizontal") * acceleration;
         rb.linearVelocity = new Vector2(x_speed, rb.linearVelocity.y);
         
-        if (x_speed !=0) animator.SetBool("running",true);
+        if (x_speed != 0) animator.SetBool("running",true);
         else animator.SetBool("running", false);
         
         if (x_speed > 0) sr.flipX = false;
@@ -51,14 +53,11 @@ public class player_movement : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             Grounded = false;
         }
-    } 
-    
-    private void OnCollisionEnter2D(Collision2D collision)
+    }
+
+    private void isGrounded()
     {
-        if (collision.collider.CompareTag("ground"))
-        {
-            Grounded = true;
-        }
+            Grounded = Physics2D.OverlapCircle(new Vector2(transform.position.x,transform.position.y-1.15f/2),0.6100233f/2,groundLayer);
     }
 
     private IEnumerator dash()
@@ -69,7 +68,7 @@ public class player_movement : MonoBehaviour
         rb.linearVelocity = new Vector2(x_speed*2, 0);
         yield return new WaitForSeconds(0.2f);
         Dashing = false;
-        rb.gravityScale =1f;
+        rb.gravityScale =2f;
         yield return new WaitForSeconds(1);
         canDash = true;
     }
